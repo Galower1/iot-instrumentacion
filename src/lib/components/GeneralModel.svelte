9bf12c0082e7bf34ce0fc27984187bce92b8e055
Galower1 <ariosgalower@gmail.com>
2023-11-11 18:10:59 -0400

    models

--- src/lib/components/GeneralModel.svelte
+++ src/lib/components/GeneralModel.svelte
@@ -2,6 +2,7 @@
   import Gauge from "./Gauge.svelte";
   import Thermometer from "./Thermometer.svelte";
   import Humidity from "./Humidity.svelte";
+  import { modulesStore } from "../stores/modulesStore";
 
   const modelImage =
     "https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaBudWC7pUMGjClqTUhgX9P0QncaRGOMuJW8mUTmnw550YAXP9k1WgMpEXpbPQ9hES55cz071rXvSdzuUnj8R2Ipek8HCQ=w2346-h1480";
@@ -73,15 +74,15 @@
 <div class="general-model">
   <div class="wrapper">
     <img class="schema" src={modelImage} alt="general-model" />
-    <Gauge name="W1 #001" style={weightStyleOne} />
-    <Gauge name="W1 #003" style={weightStyleTwo} />
-    <Gauge name="W1 #007" style={weightStyleThree} />
-    <Gauge name="PT #009" style={pressureStyleOne} />
-    <Thermometer name="TI #004" style={temperatureStyleOne} />
-    <Thermometer name="TI #005" style={temperatureStyleTwo} />
-    <Thermometer name="TI #008" style={temperatureStyleThree} />
-    <Humidity name="LS #002" style={humidityStyleOne} />
-    <Humidity name="MI #006" style={humidityStyleTwo} />
+    <Gauge value={$modulesStore.Weights[0]} name="W1 #001" style={weightStyleOne} />
+    <Gauge value={$modulesStore.Weights[1]} name="W1 #003" style={weightStyleTwo} />
+    <Gauge value={$modulesStore.Weights[2]} name="W1 #007" style={weightStyleThree} />
+    <Gauge value={$modulesStore.Pressure} name="PT #009" style={pressureStyleOne} />
+    <Thermometer value={$modulesStore.temperatures[0]} name="TI #004" style={temperatureStyleOne} />
+    <Thermometer value={$modulesStore.temperatures[1]} name="TI #005" style={temperatureStyleTwo} />
+    <Thermometer value={$modulesStore.temperatures[2]} name="TI #008" style={temperatureStyleThree} />
+    <Humidity value={$modulesStore.Water_Level} name="LS #002" style={humidityStyleOne} />
+    <Humidity value={$modulesStore.Humidity} name="MI #006" style={humidityStyleTwo} />
   </div>
 </div>
 
