3cc75422c8e5470694961a180bef2f1e4abdb4e9
Galower1 <ariosgalower@gmail.com>
2023-11-12 00:51:12 -0400

    all graphs implemented

--- src/lib/components/Socket.svelte
+++ src/lib/components/Socket.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { initWebSocket } from "../socket";
-  import { modulesStore, type ModulesStoreState } from "../stores/modulesStore";
 
   const PORT = 81;
   const HOST = window.location.hostname;
--- src/lib/pages/Variables.svelte
+++ src/lib/pages/Variables.svelte
@@ -2,8 +2,8 @@
   import "@carbon/charts-svelte/styles.css";
   import { ChartTheme, LineChart, ScaleTypes, type LineChartOptions } from "@carbon/charts-svelte";
   import { modulesStore } from "../stores/modulesStore";
-
-  let tick = 0;
+  import { onMount } from "svelte";
+  import BackButton from "../components/BackButton.svelte";
 
   type TemperatureHumidityCoeficient = {
     group: string;
@@ -11,13 +11,21 @@
     time: Date;
   };
 
-  let temperatureCoeficientValues: TemperatureHumidityCoeficient[] = [
-    {
-      group: "Grupo 1",
-      value: 0,
-      time: new Date(),
-    },
-  ];
+  type PressureVsTime = TemperatureHumidityCoeficient;
+
+  type TemperatureVsTime = PressureVsTime;
+
+  const defaultGroup = {
+    group: "Grupo 1",
+    value: 0,
+    time: new Date(),
+  };
+
+  let temperatureCoeficientValues: TemperatureHumidityCoeficient[] = [defaultGroup];
+
+  let pressureVsTimeValues: PressureVsTime[] = [defaultGroup];
+
+  let temperatureVsTimeValues: TemperatureVsTime[] = [defaultGroup];
 
   const temperatureHumidityGraph: LineChartOptions = {
     title: "Coeficiente Temperatura-Humedad Vs. Tiempo",
@@ -38,32 +46,86 @@
     theme: ChartTheme.G100,
   };
 
+  const pressureVsTime: LineChartOptions = {
+    title: "Presión vs Tiempo",
+    axes: {
+      bottom: {
+        title: "Tiempo",
+        mapsTo: "time",
+        scaleType: ScaleTypes.TIME,
+      },
+      left: {
+        title: "Presión",
+        mapsTo: "value",
+        scaleType: ScaleTypes.LINEAR,
+      },
+    },
+    curve: "curveMonotoneX",
+    height: "400px",
+    theme: ChartTheme.G100,
+  };
+
+  const temperatureVsTime: LineChartOptions = {
+    title: "Temperatura vs Tiempo",
+    axes: {
+      bottom: {
+        title: "Tiempo",
+        mapsTo: "time",
+        scaleType: ScaleTypes.TIME,
+      },
+      left: {
+        title: "Temperatura",
+        mapsTo: "value",
+        scaleType: ScaleTypes.LINEAR,
+      },
+    },
+    curve: "curveMonotoneX",
+    height: "400px",
+    theme: ChartTheme.G100,
+  };
+
   const getTemperatureHumidityCoeficient = (temperature: number, humidity: number): TemperatureHumidityCoeficient => {
     return {
       group: "Grupo 1",
-      value: (temperature * 100) / humidity,
+      value: (temperature * 100) / (humidity || 1),
       time: new Date(),
     };
   };
 
-  modulesStore.subscribe((data) => {
-    setTimeout(() => {
-      temperatureCoeficientValues.push(getTemperatureHumidityCoeficient(data.temperatures[1], data.Humidity));
-      temperatureCoeficientValues = temperatureCoeficientValues;
-    }, 3000);
-    tick++;
+  onMount(() => {
+    const interval = setInterval(() => {
+      temperatureCoeficientValues = [
+        ...temperatureCoeficientValues,
+        getTemperatureHumidityCoeficient($modulesStore.temperatures[1], $modulesStore.Humidity),
+      ];
+
+      pressureVsTimeValues = [
+        ...pressureVsTimeValues,
+        { group: "Grupo 1", value: $modulesStore.Pressure, time: new Date() },
+      ];
+
+      temperatureVsTimeValues = [
+        ...temperatureVsTimeValues,
+        { group: "Grupo 1", value: $modulesStore.temperatures[2], time: new Date() },
+      ];
+    }, 800);
+
+    return () => clearInterval(interval);
   });
 </script>
 
 <div class="graphs">
   <h1>Graphs</h1>
+  <BackButton />
   <LineChart options={temperatureHumidityGraph} data={temperatureCoeficientValues} />
+  <LineChart options={pressureVsTime} data={pressureVsTimeValues} />
+  <LineChart options={temperatureVsTime} data={temperatureVsTimeValues} />
 </div>
 
 <style>
   .graphs {
     display: flex;
-    align-self: center;
+    align-items: center;
     flex-direction: column;
     justify-content: center;
   }
